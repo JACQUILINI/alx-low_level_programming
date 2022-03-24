104dd9a81ebb8e291d3d16536ea176117101447a
Jacqilini <joshuaemmanuel072@gmail.com>
2022-03-23 20:18:50 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *swap_int - funtion to swap two integers
+ *@a: first int
+ *@b: Second int
+ *return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int hold;
+	hold = *a;
+	*a = *b;
+	*b = hold;
+}
