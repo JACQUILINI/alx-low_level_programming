55756aec954632e820fbc09a5c7d5fdd5ef850bb
Jacqilini <joshuaemmanuel072@gmail.com>
2022-03-23 21:06:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**Description: find length of a string.
+ *
+ * return: always 0.
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+	return (len);
+}
