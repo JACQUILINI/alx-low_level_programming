5f0f359b49231a2cdfc9ccc1f099c5e780466c70
Jacqilini <joshuaemmanuel072@gmail.com>
2022-03-31 01:36:50 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(void)
+{
+       int putchar();
+        return (0);
+}
